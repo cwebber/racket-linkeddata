595a38d7063465966ad06a5f3c936633917516a2
Christopher Lemmer Webber <cwebber@dustycloud.org>
2017-12-16 01:06:27 -0600

    Add copyright header to json-ld.rkt

--- json-ld.rkt
+++ json-ld.rkt
@@ -1,5 +1,21 @@
 #lang racket
 
+;;; racket-linkeddata --- Linked data tooling for Racket
+;;; Copyright © 2017 Christopher Allan Webber <cwebber@dustycloud.org>
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU Lesser General Public License as
+;;; published by the Free Software Foundation, either version 3 of the
+;;; License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU Lesser General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU Lesser General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (require json
          net/url)
 
