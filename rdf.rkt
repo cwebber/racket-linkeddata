de0d03a6818989fb4009b63c2d28861d36d18338
Christopher Lemmer Webber <cwebber@dustycloud.org>
2018-05-29 11:54:02 -0500

    Add rdf/rdfs/xsd uris and common properties

--- rdf.rkt
+++ rdf.rkt
@@ -29,3 +29,31 @@
          literal? literal-lexical-form
          literal-datatype-iri literal-language-tag)
 
+(define rdf-uri
+  "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+(define rdf:type
+  (string-append rdf-uri "type"))
+(define rdf:langString
+  (string-append rdf-uri "langString"))
+(define rdf:nil
+  (string-append rdf-uri "nil"))
+(define rdf:first
+  (string-append rdf-uri "first"))
+(define rdf:rest
+  (string-append rdf-uri "rest"))
+(define rdfs-uri
+  "http://www.w3.org/2000/01/rdf-schema#")
+(define xsd-uri
+  "http://www.w3.org/2001/XMLSchema#")
+(define xsd:boolean
+  (string-append xsd-uri "boolean"))
+(define xsd:double
+  (string-append xsd-uri "double"))
+(define xsd:integer
+  (string-append xsd-uri "integer"))
+(define xsd:string
+  (string-append xsd-uri "string"))
+
+(provide rdf-uri rdf:type rdf:langString rdf:nil rdf:first rdf:rest
+         rdfs-uri
+         xsd-uri xsd:boolean xsd:double xsd:integer xsd:string)
