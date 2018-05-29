7c36143c237a209c610f4d78e1d530d31bf86386
Christopher Lemmer Webber <cwebber@dustycloud.org>
2018-05-29 11:18:05 -0500

    Initial working version of n-quads lexer

---
+++ n-quads.rkt
@@ -0,0 +1,177 @@
+#lang racket
+
+(require parser-tools/lex
+         (prefix-in sre- parser-tools/lex-sre)
+         "rdf.rkt")
+
+;; Productions for terminals
+
+;;   HEX            ::=  [0-9] | [A-F] | [a-f]
+(define-lex-abbrev hex
+  (sre-or (char-range #\0 #\9)
+          (char-range #\A #\F)
+          (char-range #\a #\f)))
+
+;;   UCHAR          ::=  '\u' HEX HEX HEX HEX | '\U' HEX HEX HEX HEX HEX HEX HEX HEX
+(define-lex-abbrev uchar
+  (sre-or (sre-: "\\u" hex hex hex hex)
+          (sre-: "\\U" hex hex hex hex hex hex hex hex)))
+;;   ECHAR          ::=  '\' [tbnrf"'\]
+(define-lex-abbrev echar
+  (sre-: #\\ (sre-or #\t #\b #\n #\r #\f #\" #\' #\\)))
+
+;;   LANGTAG        ::=  '@' [a-zA-Z]+ ('-' [a-zA-Z0-9]+)*
+
+(define-lex-abbrev langtag
+  (sre-: #\@
+         (sre-+ (sre-or (char-range #\a #\z)
+                        (char-range #\A #\Z))
+                (sre-* (sre-+ (sre-or (char-range #\a #\z)
+                                      (char-range #\A #\Z)
+                                      (char-range #\0 #\9)))))))
+;;   EOL            ::=  [#xD#xA]+
+(define-lex-abbrev eol
+  (sre-+ (sre-or #\newline #\return)))
+
+;;   IRIREF         ::=  '<' ([^#x00-#x20<>"{}|^`\] | UCHAR)* '>'
+(define-lex-abbrev iriref
+  (sre-: #\<
+         (sre-* (sre-or
+                 (char-complement (sre-or (char-range #\u00 #\u20)
+                                          #\< #\>
+                                          #\" #\{ #\} #\| #\` #\\))
+                 uchar))
+         #\>))
+
+;;   STRING_LITERAL_QUOTE  ::=  '"' ([^#x22#x5C#xA#xD] | ECHAR | UCHAR)* '"'
+
+(define-lex-abbrev string-literal-quote
+  (sre-: #\"
+         (sre-* (sre-or (char-complement (sre-or #\" #\\ #\newline #\return))
+                        echar uchar))
+         #\"))
+
+;;   PN_CHARS_BASE  ::=  [A-Z] | [a-z] | [#x00C0-#x00D6] |
+;;                       [#x00D8-#x00F6] |
+;;                       [#x00F8-#x02FF] |
+;;                       [#x0370-#x037D] |
+;;                       [#x037F-#x1FFF] |
+;;                       [#x200C-#x200D] |
+;;                       [#x2070-#x218F] |
+;;                       [#x2C00-#x2FEF] |
+;;                       [#x3001-#xD7FF] |
+;;                       [#xF900-#xFDCF] |
+;;                       [#xFDF0-#xFFFD] |
+;;                       [#x10000-#xEFFFF]
+(define-lex-abbrev pn-chars-base
+  (sre-or (char-range #\a #\z)
+          (char-range #\A #\Z)
+          (char-range #\u00C0 #\u00D6)
+          (char-range #\u00D8 #\u00F6)
+          (char-range #\u00F8 #\u02FF)
+          (char-range #\u0370 #\u037D)
+          (char-range #\u037F #\u1FFF)
+          (char-range #\u200C #\u200D)
+          (char-range #\u2070 #\u218F)
+          (char-range #\u2C00 #\u2FEF)
+          (char-range #\u3001 #\uD7FF)
+          (char-range #\uF900 #\uFDCF)
+          (char-range #\uFDF0 #\uFFFD)
+          (char-range #\U00010000 #\U000EFFFF)))
+
+;;   PN_CHARS_U     ::=  PN_CHARS_BASE | '_' | ':'
+(define-lex-abbrev pn-chars-u
+  (sre-or pn-chars-base
+          #\_ #\:))
+
+;;   PN_CHARS       ::=  PN_CHARS_U | '-' | [0-9] | #x00B7 |
+;;                       [#x0300-#x036F] |
+;;                       [#x203F-#x2040]
+(define-lex-abbrev pn-chars
+  (sre-or pn-chars-u #\-
+          (char-range #\0 #\9)
+          #\u00B7
+          (char-range #\u0300 #\u036F)
+          (char-range #\u203F #\u2040)))
+
+;;   BLANK_NODE_LABEL  ::=  '_:' (PN_CHARS_U | [0-9]) ((PN_CHARS | '.')* PN_CHARS)?
+(define-lex-abbrev blank-node-label
+  (sre-: "_:"
+         (sre-or pn-chars-u (char-range #\0 #\9))
+         (sre-? (sre-* (sre-or pn-chars #\.)) pn-chars)))
+
+;;   nquadsDoc  ::=  statement? (EOL statement)* EOL?
+;;   statement  ::=  subject predicate object
+;;                   graphLabel? '.'
+;;   subject    ::=  IRIREF | BLANK_NODE_LABEL
+;;   predicate  ::=  IRIREF
+;;   object     ::=  IRIREF | BLANK_NODE_LABEL |
+;;                   literal
+;;   graphLabel ::=  IRIREF | BLANK_NODE_LABEL
+;;   literal    ::=  STRING_LITERAL_QUOTE ('^^' IRIREF | LANGTAG)?
+
+;; @@: Are we abusing the term token when two values?
+(struct literal-token
+  (str tag) #:transparent)
+(struct iriref-token
+  (iri) #:transparent)
+(struct langtag-token
+  (lang) #:transparent)
+(struct typetag-token
+  (iri) #:transparent)
+
+(define (lex-nquads in-port)
+  (letrec ([strip-endchars
+            (lambda (str)
+              (substring str 1
+                         (- (string-length str) 1)))]
+           [string-lexer
+            (lexer
+             [(sre-* (sre-or (char-complement (sre-or #\" #\\ #\newline #\return))
+                             echar uchar))
+              (begin
+                (unless (eq? (read-char input-port) #\")
+                  (error "Missing end quote"))
+                lexeme)])]
+           ;; we'll already have eaten the @ by now
+           [langtag-lexer
+            (lexer
+             [(sre-:
+               (sre-+ (sre-or (char-range #\a #\z)
+                              (char-range #\A #\Z)))
+               (sre-* (sre-+ (sre-or (char-range #\a #\z)
+                                     (char-range #\A #\Z)
+                                     (char-range #\0 #\9)))))
+              lexeme])]
+           [literal-lang-or-type-tag
+            (lambda (input-port)
+              (match (read-char input-port)
+                [#\@ (langtag-token (langtag-lexer input-port))]
+                [#\^
+                 (begin
+                   (unless (eq? (read-char input-port) #\^)
+                     (error "Missing a ^"))
+                   (typetag-token
+                    ((lexer
+                      [iriref (strip-endchars lexeme)])
+                     input-port)))]
+                [#\space #f]))]
+           [this-lexer
+            (lexer
+             [eol (cons 'eol
+                        (this-lexer input-port))]
+             [iriref (cons (iriref-token (strip-endchars lexeme))
+                           (this-lexer input-port))]
+             [blank-node-label (cons (cons 'blank-node-label lexeme)
+                                     (this-lexer input-port))]
+             [#\"
+              (cons (literal-token
+                     (string-lexer input-port)
+                     (literal-lang-or-type-tag input-port))
+                    (this-lexer input-port))]
+             [#\. (cons 'dot
+                        (this-lexer input-port))]
+             [(sre-+ #\space) (this-lexer input-port)]
+             [(eof) '(eof)])])
+    (this-lexer in-port)))
+
