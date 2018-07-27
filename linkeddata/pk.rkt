a631274c46bd963180f245412c588e29cd8965c9
Christopher Lemmer Webber <cwebber@dustycloud.org>
2018-07-27 13:21:35 -0400

    Add pk.rkt

---
+++ linkeddata/pk.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+;; "peeking" utilities
+
+;; TODO: Move this into its own library
+
+(provide pk pk-values)
+
+;; kludge: emacs mis-renders this as a string, so...
+(define hash-semicolon-semicolon
+  (list->string '(#\# #\; #\;)))
+
+;; for debugging
+(define (pk . vals)
+  "Peek at values for print debugging, but return 'em"
+  (display hash-semicolon-semicolon)
+  (display " pk\n")
+  (write vals)
+  (newline)
+  ;; return the last value
+  (last vals))
+
+(define-syntax-rule (pk-values print-these ... body)
+  ;; Like pk, but supporting multiple value return
+  (call-with-values
+      (lambda () body)
+    (lambda vals
+      (display hash-semicolon-semicolon)
+      (display " pk-values\n")
+      (pretty-print (list print-these ... '*values:* vals))
+      (apply values vals))))
